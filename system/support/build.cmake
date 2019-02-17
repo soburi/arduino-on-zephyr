347880244662f46ce05167aa8970bf981859b158
soburi <soburi@gmail.com>
2019-02-17 20:35:25 +0900

    Tune preproc processing time

--- system/support/build.cmake
+++ system/support/build.cmake
@@ -4,16 +4,20 @@ set(build_dir ${ARDUINO_BUILD_PATH})
 
 file(COPY ${ARDUINO_VARIANT_PATH}/prj.conf DESTINATION ${ARDUINO_BUILD_PATH}/_cmakefile )
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -GNinja -DBOARD=${BOARD} _cmakefile
-  WORKING_DIRECTORY ${build_dir}
-)
+if(EXISTS ${ARDUINO_BUILD_PATH}/_cmakefile/.NOT_CHANGED )
+  file(REMOVE ${ARDUINO_BUILD_PATH}/_cmakefile/.NOT_CHANGED )
+else()
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -GNinja -DBOARD=${BOARD} _cmakefile
+    WORKING_DIRECTORY ${build_dir}
+  )
+endif()
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} --build ${build_dir} -- 
-  WORKING_DIRECTORY ${build_dir}
-  RESULT_VARIABLE retcode
-)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} --build ${build_dir} --
+    WORKING_DIRECTORY ${build_dir}
+    RESULT_VARIABLE retcode
+  )
 
 if(${retcode})
   message(FATAL_ERROR)
--- system/support/preproc.cmake
+++ system/support/preproc.cmake
@@ -4,19 +4,25 @@ set(preproc_dir ${ARDUINO_BUILD_PATH}/preproc)
 
 file(COPY ${ARDUINO_VARIANT_PATH}/prj.conf DESTINATION ${ARDUINO_BUILD_PATH}/preproc/_cmakefile )
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -GNinja -DBOARD=${BOARD} _cmakefile
-  WORKING_DIRECTORY ${preproc_dir}
-  OUTPUT_QUIET
-  ERROR_QUIET
-)
+if(EXISTS ${ARDUINO_BUILD_PATH}/preproc/_cmakefile/.NOT_CHANGED )
+  file(REMOVE ${ARDUINO_BUILD_PATH}/preproc/_cmakefile/.NOT_CHANGED )
+else()
+  if(NOT EXISTS ${ARDUINO_BUILD_PATH}/preproc/preproc.sh )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -GNinja -DBOARD=${BOARD} _cmakefile
+      WORKING_DIRECTORY ${preproc_dir}
+      OUTPUT_QUIET
+      ERROR_QUIET
+    )
 
-execute_process(
-  COMMAND ${CMAKE_COMMAND} --build ${preproc_dir} -- zephyr/linker.cmd
-  WORKING_DIRECTORY ${preproc_dir}
-  OUTPUT_QUIET
-  ERROR_QUIET
-)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} --build ${preproc_dir} -- zephyr/linker.cmd
+      WORKING_DIRECTORY ${preproc_dir}
+      OUTPUT_QUIET
+      ERROR_QUIET
+    )
+  endif()
+endif()
 
 set(run_preproc_script bash ${ARDUINO_BUILD_PATH}/preproc/preproc.sh)
 
