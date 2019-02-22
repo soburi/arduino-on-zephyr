9fced94353797383340a9485f78950851e54bc46
soburi <soburi@gmail.com>
2019-02-23 08:04:21 +0900

    Add post_install.bat

--- dist/make_case_sensitive.ps1
+++ dist/make_case_sensitive.ps1
@@ -1,1 +1,13 @@
-(gci -Recurse -Directory).FullName | % {fsutil.exe file setCaseSensitiveInfo $_ enable}
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  $arglist = "-ExecutionPolicy","RemoteSigned","-File","$PSCommandPath"
+  $arglist += $Args[0]
+
+  Start-Process powershell.exe -Wait -Verb RunAs -ArgumentList $arglist
+  exit
+}
+else
+{
+  (gci -Path $Args[0] -Recurse -Directory).FullName | % {fsutil.exe file setCaseSensitiveInfo $_ enable}
+}
--- post_install.bat
+++ post_install.bat
@@ -1,8 +1,2 @@
 @echo off
-net file > NUL 2>&1
-if NOT %ERRORLEVEL% EQU 0 (
-    powershell start-process powershell %~dp0\dist\make_case_sensitive.ps1 -Verb RunAs
-) else (
-    powershell start-process powershell %~dp0\dist\make_case_sensitive.ps1
-)
-
+powershell -ExecutionPolicy RemoteSigned powershell %~dp0\dist\make_case_sensitive.ps1 %~dp0
