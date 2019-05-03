67d8a8f1b7007ce0b39278d6b7ecc6fb334c75b6
soburi <soburi@gmail.com>
2019-05-04 00:24:06 +0900

    Define yield() function as alias of k_yeild()

--- cores/scirocco/Arduino.h
+++ cores/scirocco/Arduino.h
@@ -29,7 +29,6 @@
 #endif
 
 #include <zephyr.h>
-//#include <board.h>
 #include <device.h>
 #include <gpio.h>
 
@@ -60,7 +59,7 @@ extern "C"{
 #define clockCyclesToMicroseconds(a) ( ((a) * 1000L) / (SystemCoreClock / 1000L) )
 #define microsecondsToClockCycles(a) ( (a) * (SystemCoreClock / 1000000L) )
 
-void yield( void ) ;
+static inline void yield( void ) { k_yield(); }
 
 /* system functions */
 int main( void );
